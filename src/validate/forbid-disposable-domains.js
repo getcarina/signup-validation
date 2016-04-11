8e9bbf19dce8c80e4c6d2be5c7e497812ae85655
Keith Bartholomew <keith.bartholomew@rackspace.com>
2016-04-11 14:14:35 -0500

    lowercase the email domain prior to checking against domain list

--- src/validate/forbid-disposable-domains.js
+++ src/validate/forbid-disposable-domains.js
@@ -2,7 +2,10 @@ const blacklistedDomains = require('disposable-email-domains');
 const Q = require('q');
 
 module.exports = (input) => {
-  if (input.match(/@(.+)$/) && blacklistedDomains.indexOf(input.match(/@(.+)$/)[1]) !== -1) {
+  if (
+    input.match(/@(.+)$/) && 
+    blacklistedDomains.indexOf(input.match(/@(.+)$/)[1].toLowerCase()) !== -1
+  ) {
     return Q.reject('A username cannot be an e-mail address from that domain.');
   }
 
